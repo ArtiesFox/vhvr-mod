f091ec6ba3a24e84e2b9c389394f3dff725c9572
ArtiesFox <thanapat.intarasorn@gmail.com>
2023-09-18 14:42:48 +0700

    [nonVR][UI] tooltip not show fixed

--- ValheimVRMod/Patches/ControlPatches.cs
+++ ValheimVRMod/Patches/ControlPatches.cs
@@ -749,6 +749,11 @@ namespace ValheimVRMod.Patches {
     {
         static bool Prefix(InventoryGrid __instance, ref InventoryGrid.Element __result)
         {
+            if (VHVRConfig.NonVrPlayer())
+            {
+                return true;
+            }
+            
             foreach (InventoryGrid.Element element in __instance.m_elements)
             {
                 RectTransform rectTransform = element.m_go.transform as RectTransform;
